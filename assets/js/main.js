99857b55d861e8e1b5cd9f111482f3647faa945a
rasan3-1416 <md.rakibhasan3.1416@gmail.com>
2023-10-05 17:12:27 +0600

    feat(nav): add js script for header/nav
    added javascript in js/main.js file for making the navigation dynamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,37 @@
 /*=============== SHOW MENU ===============*/
-
-
+const navMenu = document.getElementById('nav-menu'),
+    navClose = document.getElementById('nav-close'),
+    navToggle = document.getElementById('nav-toggle')
+/*======== Menu Show ========*/
+/* Validates if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*======== Menu Hide ========*/
+/* Validates if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*======== Remove Menu Mobile ========*/
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () => {
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
 /*=============== ADD BLUR HEADER ===============*/
 
 
-/*=============== SWIPER FAVORITES ===============*/ 
+/*=============== SWIPER FAVORITES ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
